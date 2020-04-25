91d2194289731b8b6aa555baeea0bea13b2b1423
xiayoutao <1425244634@qq.com>
2020-04-25 14:27:36 +0800

    fix: postMessage域名问题

--- src/scripts/tools.js
+++ src/scripts/tools.js
@@ -1,7 +1,7 @@
 import store from '@/store';
 
 // 发送消息
-export function postMessage (theWindow, data, delay = 0, origin = '*') {
+export function postMessage (theWindow, data, delay = 0, origin) {
   origin = origin || store.state.previewOrigin;
   setTimeout(() => {
     theWindow.postMessage(data, 'http://localhost:7001');
