9dae069194de5a3db6d72424f3a0cfdfad224c5b
xiayoutao <1425244634@qq.com>
2020-04-30 13:40:47 +0800

    feat: 新增身份证校验方法

--- src/scripts/validate.js
+++ src/scripts/validate.js
@@ -3,7 +3,7 @@
  * @param {*} s
  */
 export function isEmail (s) {
-  return /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((.[a-zA-Z0-9_-]{2,3}){1,2})$/.test(s);
+  return /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((.[a-zA-Z0-9_-]{2, 3}){1, 2})$/.test(s);
 }
 
 /**
@@ -19,7 +19,7 @@ export function isMobile (s) {
  * @param {*} s
  */
 export function isPhone (s) {
-  return /^([0-9]{3,4}-)?[0-9]{7,8}$/.test(s);
+  return /^([0-9]{3, 4}-)?[0-9]{7,8}$/.test(s);
 }
 
 /**
@@ -31,7 +31,7 @@ export function isURL (s) {
 }
 
 export function isIP (s) {
-  return /(?=(\b|\D))(((\d{1,2})|(1\d{1,2})|(2[0-4]\d)|(25[0-5]))\.){3}((\d{1,2})|(1\d{1,2})|(2[0-4]\d)|(25[0-5]))(?=(\b|\D))/.test(s);
+  return /(?=(\b|\D))(((\d{1, 2})|(1\d{1, 2})|(2[0-4]\d)|(25[0-5]))\.){3}((\d{1, 2})|(1\d{1, 2})|(2[0-4]\d)|(25[0-5]))(?=(\b|\D))/.test(s);
 }
 
 /**
@@ -66,7 +66,7 @@ export function decimal (s, num) {
  * @param {*} s
  */
 export function isMoney (s) {
-  var reg = /^(([1-9][0-9]*)|(([0]\.\d{1,2}|[1-9][0-9]*\.\d{1,2})))$/;
+  var reg = /^(([1-9][0-9]*)|(([0]\.\d{1, 2}|[1-9][0-9]*\.\d{1, 2})))$/;
   return reg.test(s);
 }
 
@@ -97,4 +97,40 @@ export function getStrLength (s, flag) {
   } else {
     return s.length;
   }
+}
+
+/**
+ * 判断身份证号码是否正确
+ * @param {*} idcard 身份证号码
+ */
+export function isID (idcard) {
+  let pass = true; // 是否验证通过
+  const city = { 11: '北京', 12: '天津', 13: '河北', 14: '山西', 15: '内蒙古', 21: '辽宁', 22: '吉林', 23: '黑龙江 ', 31: '上海', 32: '江苏', 33: '浙江', 34: '安徽', 35: '福建', 36: '江西', 37: '山东', 41: '河南', 42: '湖北 ', 43: '湖南', 44: '广东', 45: '广西', 46: '海南', 50: '重庆', 51: '四川', 52: '贵州', 53: '云南', 54: '西藏 ', 61: '陕西', 62: '甘肃', 63: '青海', 64: '宁夏', 65: '新疆', 71: '台湾', 81: '香港', 82: '澳门', 91: '国外' };
+  let errorTips = '';
+  if (!idcard || !/^\d{6}(18|19|20)?\d{2}(0[1-9]|1[012])(0[1-9]|[12]\d|3[01])\d{3}(\d|X)$/i.test(idcard)) {
+    errorTips = '身份证号格式错误';
+    pass = false;
+  } else if (!city[idcard.substr(0, 2)]) {
+    errorTips = '地址编码错误';
+    pass = false;
+  } else if (idcard.length === 18) {
+    const factor = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]; // 加权因子, ∑(ai×Wi)(mod 11)
+    const parity = [1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2]; // 校验位
+    let sum = 0;
+    let ai = 0;
+    let wi = 0;
+    const codes = idcard.split('');
+    codes.forEach((item, index) => {
+      if (index < 17) {
+        ai = codes[index];
+        wi = factor[index];
+        sum += ai * wi;
+      }
+    });
+    if (parity[sum % 11] != codes[17]) {
+      errorTips = '校验位错误';
+      pass = false;
+    }
+  }
+  return pass;
 }
