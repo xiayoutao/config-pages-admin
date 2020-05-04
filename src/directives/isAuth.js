307f14260eed9b9c9f67c8c6368bbceedbea214e
xiayoutao <1425244634@qq.com>
2020-05-04 11:58:41 +0800

    perf: 优化权限指令

--- src/directives/isAuth.js
+++ src/directives/isAuth.js
@@ -1,5 +1,3 @@
-const permissions = (sessionStorage.getItem('permissions') || '').split(','); // 拿到sessionStorage中保存的权限标识值，并根据,分割成数组
-
 export default {
   // 指令第一次绑定到元素时调用
   bind (el, binding) {
@@ -7,6 +5,7 @@ export default {
   },
   // 被绑定元素插入父节点时调用
   inserted (el, binding) {
+    const permissions = (sessionStorage.getItem('permissions') || '').split(','); // 拿到sessionStorage中保存的权限标识值，并根据,分割成数组
     const hasAuth = permissions.indexOf(binding.value) >= 0; // 判断当前指令中的值是否存在于permissions中
     if (hasAuth) { // 有权限
       el.style.display = ''; // 显示元素
@@ -15,7 +14,7 @@ export default {
     }
   },
   // 所在组件的 VNode 更新时调用
-  update () {},
+  update (el, binding) {},
   // 指令所在组件的 VNode 及其子 VNode 全部更新后调用
   componentUpdated () {},
   // 指令与元素解绑时调用
